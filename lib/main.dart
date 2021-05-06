ef2ee56c2bfb0fdbca4967a97cd406324b4db11e
Stefan Iseli <stefan.iseli@hotmail.ch>
2021-05-06 15:21:03 +0900

    sql.dart added

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:mysql1/mysql1.dart';
+//import 'package:mysql1/mysql1.dart';
+import 'package:tearrific/sql.dart';
 
-Future main() async {
-  final conn = await MySqlConnection.connect(ConnectionSettings(
-      host: '35.188.216.13',
-      port: 3306,
-      user: 'tearrific',
-      password: 'opaHFjs07eB7j8dm',
-      db: 'db_tearrific'));
-
-  var results = await conn.query(
-      'select p.No, c.Name, p.Name, p.Currency, p.Buy_Price from Product_Categories c, Products p where p.Product_Category = c.No order by p.No;');
-  for (var row in results) {
-    print(
-        'No: ${row[0]}, Category: ${row[1]}, Product Name: ${row[2]}, Currency: ${row[3]}, Buy Price: ${row[4]}');
-  }
-
-  await conn.close();
-
+void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  final conn = mySQLConnectService.mySQLConnect();
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
